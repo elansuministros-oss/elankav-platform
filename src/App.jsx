34ded5ad892cf48a207d52f0688508e50a354f32
elansuministros-oss <elansuministros@gmail.com>
2026-08-30 17:34:52 -0600

    feat(elan-go): surface autonomous broker status

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero"
 import Essence from "./components/Essence"
 import Projects from "./components/Projects"
 import Footer from "./components/Footer"
+import ElanGoStatus from "./components/ElanGoStatus"
 import PublicReceipt from "./components/PublicReceipt"
 import SellerReferralLanding from "./components/SellerReferralLanding"
 
@@ -36,6 +37,7 @@ export default function App() {
       <Hero />
       <Essence />
       <Projects />
+      <ElanGoStatus />
       <Footer />
     </>
   )
