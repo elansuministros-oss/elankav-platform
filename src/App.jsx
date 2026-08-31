3262cb85e990e16eaefac5c7f79a0ebd9bfda880
elansuministros-oss <elansuministros@gmail.com>
2026-08-30 18:26:09 -0600

    fix(elan-go): remove status panel from public portal

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Hero from "./components/Hero"
 import Essence from "./components/Essence"
 import Projects from "./components/Projects"
 import Footer from "./components/Footer"
-import ElanGoStatus from "./components/ElanGoStatus"
 import PublicReceipt from "./components/PublicReceipt"
 import SellerReferralLanding from "./components/SellerReferralLanding"
 
@@ -37,7 +36,6 @@ export default function App() {
       <Hero />
       <Essence />
       <Projects />
-      <ElanGoStatus />
       <Footer />
     </>
   )
